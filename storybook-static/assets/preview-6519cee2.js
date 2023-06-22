eb03cfc6e231f7d9714f6931900adc70ec9d1746
exit-yoko <61134411+exit-yoko@users.noreply.github.com>
2023-06-22 23:16:17 +0900

    first commit

---
+++ storybook-static/assets/preview-6519cee2.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-02fcd6ad.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-776b83e8.js"),["./DocsRenderer-EYKKDMVH-776b83e8.js","./iframe-02fcd6ad.js","./index-aef01589.js","./_commonjsHelpers-042e6b4d.js","./index-d475d2ea.js","./index-d37d4223.js","./index-5e7ae662.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-6519cee2.js.map
